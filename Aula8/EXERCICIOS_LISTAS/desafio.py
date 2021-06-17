2fe881b5bb7dc8d181f0e5f7c9d1cee93cb7c0fc
menezesfelipee <sa.felipem@gmail.com>
2021-06-17 13:52:57 -0300

    exercicios 16-06

--- Aula8/EXERCICIOS_LISTAS/desafio.py
+++ Aula8/EXERCICIOS_LISTAS/desafio.py
@@ -1,9 +1,9 @@
 # Modo 1
 perguntas = [input('Telefonou para a vítima? [S/N] ').strip().upper()[0],
-input('Esteve no local do crime? ').strip().upper()[0],
-input('Mora perto da vítima? ').strip().upper()[0],
-input('Devia para a vítima? ').strip().upper()[0],
-input('Já trabalhou com a vítima? ').strip().upper()[0]]
+input('Esteve no local do crime? [S/N] ').strip().upper()[0],
+input('Mora perto da vítima? [S/N] ').strip().upper()[0],
+input('Devia para a vítima? [S/N] ').strip().upper()[0],
+input('Já trabalhou com a vítima? [S/N] ').strip().upper()[0]]
 if perguntas.count('S') == 2:
     print('Suspeita')
 elif perguntas.count('S') == 3 or perguntas.count('S') == 4:
@@ -16,10 +16,10 @@ else:
 
 # Modo 2
 perguntas = [input('Telefonou para a vítima? [S/N] ').strip().upper()[0],
-input('Esteve no local do crime? ').strip().upper()[0],
-input('Mora perto da vítima? ').strip().upper()[0],
-input('Devia para a vítima? ').strip().upper()[0],
-input('Já trabalhou com a vítima? ').strip().upper()[0]]
+input('Esteve no local do crime? [S/N] ').strip().upper()[0],
+input('Mora perto da vítima? [S/N] ').strip().upper()[0],
+input('Devia para a vítima? [S/N] ').strip().upper()[0],
+input('Já trabalhou com a vítima? [S/N] ').strip().upper()[0]]
 respostas = ['Inocente','Suspeita','Cúmplice','Cúmplice','Assassino']
 quant = perguntas.count('S') - 1
 print(f'Você é {respostas[quant]}')
