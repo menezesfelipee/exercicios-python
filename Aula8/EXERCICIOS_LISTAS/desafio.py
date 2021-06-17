e69b0b430739bf3b2f1aa7491720cabedcee4781
menezesfelipee <sa.felipem@gmail.com>
2021-06-17 14:03:16 -0300

    exercicios 16-06

--- Aula8/EXERCICIOS_LISTAS/desafio.py
+++ Aula8/EXERCICIOS_LISTAS/desafio.py
@@ -23,3 +23,16 @@ input('Já trabalhou com a vítima? [S/N] ').strip().upper()[0]]
 respostas = ['Inocente','Suspeita','Cúmplice','Cúmplice','Assassino']
 quant = perguntas.count('S') - 1
 print(f'Você é {respostas[quant]}')
+
+
+# Modo 3
+perguntas = ['Telefonou para a vítima? [S/N] ',
+'Esteve no local do crime? [S/N] ',
+'Mora perto da vítima? [S/N] ',
+'Devia para a vítima? [S/N] ',
+'Já trabalhou com a vítima? [S/N] ']
+for i in range(len(perguntas)):
+    perguntas[i] = input(perguntas[i]).strip().upper()[0]
+respostas = ['Inocente','Suspeita','Cúmplice','Cúmplice','Assassino']
+quant = perguntas.count('S') - 1
+print(f'Você é {respostas[quant]}')
