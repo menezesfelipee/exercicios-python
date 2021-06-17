ca6bd366a552ad1284b407d7381fa531b1205674
menezesfelipee <sa.felipem@gmail.com>
2021-06-17 13:27:18 -0300

    exercicios 16-06

--- Aula8/EXERCICIOS_LISTAS/ex01.py
+++ Aula8/EXERCICIOS_LISTAS/ex01.py
@@ -1,8 +1,20 @@
+# Modo 1
 l = [5, 7, 2, 9, 4, 1, 3]
 print(f'''Tamanho: {len(l)}
 Maior valor: {max(l)}
 Menor valor: {min(l)}
 Soma de todos os itens: {sum(l)}
 Lista em ordem crescente: {sorted(l)}
-Lista em ordem decrescente: {sorted(l, reverse=True)}
-''')
+Lista em ordem decrescente: {sorted(l, reverse=True)}''')
+
+
+# Modo 2
+l = [5, 7, 2, 9, 4, 1, 3]
+print(f'''Tamanho: {len(l)}
+Maior valor: {max(l)}
+Menor valor: {min(l)}
+Soma de todos os itens: {sum(l)}''')
+l.sort()
+print(f'Lista em ordem crescente: {l}')
+l.reverse()
+print(f'Lista em ordem decrescente: {l}')
