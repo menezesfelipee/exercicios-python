316afa2b32135b828b5b0bd39ae721ae9dca08f9
menezesfelipee <sa.felipem@gmail.com>
2021-06-18 09:19:24 -0300

    organizando

--- Aula8/listas_desafio.py
+++ Aula8/listas_desafio.py
@@ -34,5 +34,4 @@ perguntas = ['Telefonou para a vítima? [S/N] ',
 for i in range(len(perguntas)):
     perguntas[i] = input(perguntas[i]).strip().upper()[0]
 respostas = ['Inocente','Suspeita','Cúmplice','Cúmplice','Assassino']
-quant = perguntas.count('S') - 1
-print(f'Você é {respostas[quant]}')
+print(f'Você é {respostas[perguntas.count("S") - 1]}')
