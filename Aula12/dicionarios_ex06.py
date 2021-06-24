9c24d884e1e49524d513483e277c5844f05c8d28
menezesfelipee <sa.felipem@gmail.com>
2021-06-24 10:59:22 -0300

    exercises 6 comments

--- Aula12/dicionarios_ex06.py
+++ Aula12/dicionarios_ex06.py
@@ -11,10 +11,10 @@ for i in range(15):
     aluno['notas'] = list()
     media = 0
     for j in range(5):
-        nota = round(uniform(0,10), 2)
+        nota = round(uniform(0,10), 2)  # 'uniform' sorteia um ponto flutuante no intervalo dado, inclusive o limite superior e inferior  ||  'round' arrendoda para a quantidade de casas decimais dada
         aluno['notas'].append(nota)
-        media += (nota/5)
-    aluno['media'] = round(media, 2)
+        media += (nota/5)               # (nota1 + nota2 + nota3 + nota4 + nota5) / 5   =   (nota1 / 5) + (nota2 / 5) + (nota3 / 5) + (nota4 / 5) + (nota5 / 5)
+    aluno['media'] = round(media, 2)    # Já a arredonda a média para a 2 casas decimais
     if aluno['media'] >= 7:
         aluno['situacao'] = 'aprovado'
     elif 5 <= aluno['media'] < 7:
@@ -23,9 +23,17 @@ for i in range(15):
         aluno['situacao'] = 'reprovado'
     classe.append(aluno)
 
+## PARA FORMATAR A F-STRING PODEMOS UTILIZAR OS SÍMBOLOS A SEGUIR: ##
+# >6  : vai guardar exatamente 6 espaços para caracteres para a sua coluna, alinhando À DIREITA.
+# <20 : vai guardar exatamente 20 espaços para caracteres para a sua coluna, alinhando À ESQUERDA.
+# ^10 : vai guardar exatamente 10 espaços para caracteres para a sua coluna, alinhando AO CENTRO.
+# .2f : coloca exatamente 2 casas decimais no float, preenchendo com zeros, se necessário
+
+## VOCÊ PODE MISTURAR A FORMATAÇÃO DE ESPAÇOS E DE CASAS DECIMAIS ##
+# <15.3f : vai guardar exatamente 15 espaços para caracteres para a sua coluna, alinhando À ESQUERDA, e coloca exatamente 3 casas decimais no float
+
 print(f'{"Nome":>6} {"Notas":>37} {"Média":>30} {"Situação":>13}')
 
 for aluno in classe:
     notas = '  -  '.join([f'{n:.2f}' for n in aluno['notas']])
-    media = f"{aluno['media']:.2f}"
-    print(f'{aluno["nome"]:<20} {notas:<48} {media:<10} {aluno["situacao"]}')
+    print(f'{aluno["nome"]:<20} {notas:<48} {aluno["media"]:<10.2f} {aluno["situacao"]}')
