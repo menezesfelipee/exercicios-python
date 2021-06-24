20630cb0d35c375525e8a5f71864652c8883d327
menezesfelipee <sa.felipem@gmail.com>
2021-06-23 22:51:07 -0300

    improving exercise 6

--- Aula12/dicionarios_ex06.py
+++ Aula12/dicionarios_ex06.py
@@ -1,16 +1,19 @@
 '''6. Desafio: Continuando o exercício 3 crie agora um boletim escolar, seu programa deve receber 5 notas de 15 alunos, assim como o nome desses alunos, o programa tem que calcular a média desse aluno e mostrar a situação dele, seguindo os mesmos critérios apresentados no exercício 3. No final apresente todos nomes, as 5 notas, as médias e as situações dos 15 alunos de uma vez só.'''
 
+from random import uniform
+from names import get_first_name
+
 classe = list()
 
 for i in range(15):
     aluno = dict()
-    aluno['nome'] = input('Digite o nome: ').capitalize()
+    aluno['nome'] = get_first_name()
     aluno['notas'] = list()
     media = 0
     for j in range(5):
-        nota = float(input(f'Digite a {j+1}ª nota '))
+        nota = round(uniform(0,10), 2)
         aluno['notas'].append(nota)
-        media += nota/5
+        media += (nota/5)
     aluno['media'] = round(media, 2)
     if aluno['media'] >= 7:
         aluno['situacao'] = 'aprovado'
@@ -20,9 +23,9 @@ for i in range(15):
         aluno['situacao'] = 'reprovado'
     classe.append(aluno)
 
-print()
+print(f'{"Nome":>6} {"Notas":>37} {"Média":>30} {"Situação":>13}')
 
-print(f'{"Nome":^25} {"Notas":^25} {"Média":^25} {"Situação":^25}')
 for aluno in classe:
-    notas = ", ".join([str(n) for n in aluno['notas']])
-    print(f'{aluno["nome"]:^25} {notas:^25} {aluno["media"]:^25} {aluno["situacao"]:^25}')
+    notas = '  -  '.join([f'{n:.2f}' for n in aluno['notas']])
+    media = f"{aluno['media']:.2f}"
+    print(f'{aluno["nome"]:<20} {notas:<48} {media:<10} {aluno["situacao"]}')
