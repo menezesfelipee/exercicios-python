5c875a8a9932608f997fa269a4a47adb0f14cd99
menezesfelipee <sa.felipem@gmail.com>
2021-06-30 12:12:43 -0300

    update oop exercises

--- Aula17/oop_desafio1.py
+++ Aula17/oop_desafio1.py
@@ -1,19 +1,23 @@
-from random import randint
+# Utilizando os conceitos de Orientação a Objetos (OO) vistos na aula anterior, crie um lançador de dados e moedas em que o usuário deve escolher o objeto a ser lançado. Não esqueça que os lançamentos são feitos de forma randômica.
+
+from random import randint, choice
 
 class Lancador:
     def __init__(self):
-        self.moeda = randint(0, 1)
-        self.dado = randint(1,6)
-    def jogarMoeda(self):
-        if self.moeda == 0:
-            self.moeda = 'Cara'
+        self.moeda = choice(['Cara', 'Coroa'])
+        self.dado = randint(1, 6)
+    def jogar(self, opcao):
+        if opcao == 'M':
+            return self.moeda
+        elif opcao == 'D':
+            return self.dado
         else:
-            self.moeda = 'Coroa'
-        return self.moeda
-    def jogarDado(self):
-        return self.dado
-        
-sortear = Lancador()
-print(sortear.jogarMoeda())
-print()
-print(sortear.jogarDado())
+            return 'Opção inválida'
+
+moeda1 = Lancador()
+opcao = input('Você quer sortear moeda ou dado? [M/D] ').strip().upper()[0]
+print(f'A moeda caiu em: {moeda1.jogar(opcao)}')
+
+dado1 = Lancador()
+opcao = input('Você quer sortear moeda ou dado? [M/D] ').strip().upper()[0]
+print(f'O dado caiu em: {dado1.jogar(opcao)}')
--- Aula17/oop_desafio2.py
+++ Aula17/oop_desafio2.py
@@ -1,3 +1,7 @@
+# Vamos aprimorar o código: cadastro de jogador de futebol.py que foi desenvolvido no CodeLab da aula14. Faça com que o seu código funcione para vários jogadores, incluindo um sistema de visualização de detalhes de aproveitamento de cada jogador.
+
+# Modo 1
+
 class Jogador:
     def __init__(self, nome, partidas, golsTotal):
         self.nome = nome
@@ -9,20 +13,50 @@ class Jogador:
         Nome: {self.nome}
         Partidas: {self.partidas}
         Gols no Campeonato: {self.golsTotal}
-        Aproveitamento: {self.golsPartida:.2f}'''
+        Aproveitamento: {self.golsPartida:.2f} por partida'''
 
 def menu():
     nome = input('Digite o nome do jogador: ')
     partidas = int(input('Digite a quantidade de partidas jogadas: '))
     golsTotal = int(input('Digite a quantidade de gols: '))
-    return nome, partidas, golsTotal
+    return Jogador(nome, partidas, golsTotal)
 
-jogadores = {}
+time = dict()
 
 while True:
-    jogador = Jogador(menu())
+    jogador = menu()
+    time[jogador.nome] = jogador
     print(jogador.aproveitamento())
     continuar = input('Quer continuar cadastrando? [S/N] ').strip().upper()[0]
     if continuar == 'N':
         break
-    
+
+print(time['felipe'].aproveitamento())
+
+
+# Modo 2
+
+class Jogador:
+    def __init__(self):
+        self.nome = input('Digite o nome do jogador: ')
+        self.partidas = int(input('Digite a quantidade de partidas jogadas: '))
+        self.golsTotal = int(input('Digite a quantidade de gols: '))
+        self.golsPartida = self.golsTotal / self.partidas
+    def aproveitamento(self):
+        return f'''
+        Nome: {self.nome}
+        Partidas: {self.partidas}
+        Gols no Campeonato: {self.golsTotal}
+        Aproveitamento: {self.golsPartida:.2f} por partida'''
+
+time = dict()
+
+while True:
+    jogador = Jogador()
+    time[jogador.nome] = jogador
+    print(jogador.aproveitamento())
+    continuar = input('Quer continuar cadastrando? [S/N] ').strip().upper()[0]
+    if continuar == 'N':
+        break
+
+print(time['felipe'].aproveitamento())
